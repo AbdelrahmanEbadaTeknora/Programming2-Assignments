3653736575493e4857db9eadb85a5647e6a7d4a9
AbdelrahmanEbada <abdelrahmanebada174@gmail.com>
2025-10-29 15:26:12 +0300

    started the files cleanly

--- src/ISearchable.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface ISearchable 
-{
-    public student searchById(int id);
-    public List<student> searchByName(String name);
-    public List<student> searchByDepartment(String dept);
-}
