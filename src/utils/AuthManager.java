94cd41ee50211f858adc96d651f2c77fea1e5542
Marwan Mohamed <marwaneltete12@gmail.com>
2025-11-17 04:47:21 +0200

    packages edits

--- src/utils/AuthManager.java
+++ src/utils/AuthManager.java
@@ -1,3 +1,9 @@
+package ui;
+
+import models.Instructor;
+import models.Student;
+import models.User;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
