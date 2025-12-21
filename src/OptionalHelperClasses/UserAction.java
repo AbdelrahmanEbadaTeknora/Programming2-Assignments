88db2b92fb1d6de1568155c215f7993e100911c6
Marwan Mohamed <marwaneltete12@gmail.com>
2025-12-21 16:09:49 +0200

    handeling Packages

---
+++ src/OptionalHelperClasses/UserAction.java
@@ -0,0 +1,45 @@
+package main.java.utils;
+
+/**
+ * Encapsulates a user action for main.javaa.logging
+ */
+public class UserAction {
+    private int row;
+    private int col;
+    private int newValue;
+    private int previousValue;
+    private long timestamp;
+
+    public UserAction(int row, int col, int newValue, int previousValue) {
+        this.row = row;
+        this.col = col;
+        this.newValue = newValue;
+        this.previousValue = previousValue;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getNewValue() {
+        return newValue;
+    }
+
+    public int getPreviousValue() {
+        return previousValue;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d,%d,%d,%d)", row, col, newValue, previousValue);
+    }
+}
---
+++ src/solver/SolutionResult.java
@@ -0,0 +1,38 @@
+package main.java.Models;
+
+import java.util.List;
+
+/**
+ * Contains the solution mapping for a Sudoku puzzle
+ */
+public class SolutionResult {
+    private int[] solution;           // Array of values for empty cells
+    private List<Position> emptyPositions;  // Positions of empty cells
+    private boolean solved;
+
+    public SolutionResult(int[] solution, List<Position> emptyPositions, boolean solved) {
+        this.solution = solution;
+        this.emptyPositions = emptyPositions;
+        this.solved = solved;
+    }
+
+    public int[] getSolution() {
+        return solution;
+    }
+
+    public List<Position> getEmptyPositions() {
+        return emptyPositions;
+    }
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    @Override
+    public String toString() {
+        return "SolutionResult{" +
+                "solved=" + solved +
+                ", solutions=" + (solution != null ? solution.length : 0) +
+                '}';
+    }
+}
